a265d092668a621c4bb0bfdbf3279a2ed172d21e
shariful8523 <mdsharifulislam8523@gmail.com>
2025-05-25 04:39:18 +0600

    recive user review and send db and send client

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfpeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,35 +26,56 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const menuCollection = client.db("bistrodb").collection("menu");
     const reviewCollection = client.db("bistrodb").collection("reviews");
     const cartCollection = client.db("bistrodb").collection("carts");
-    
 
-    app.get('/menu', async(req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+      
+    // review related apis
+
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
     })
 
-    app.get('/review', async(req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+    app.post('/review', async (req, res) => {
+      const Reviews = req.body;
+      const result =await reviewCollection.insertOne(Reviews);
+      res.send(result);
     })
 
 
     // Cart related apis
 
-    app.post('/carts', async(req, res) => {
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     })
 
-    app.get('/carts', async(req, res) =>{
-      const result = await cartCollection.find().toArray();
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
 
 
 
@@ -84,9 +105,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Boss is siting')
+  res.send('Boss is siting')
 })
 
 app.listen(port, () => {
-    console.log(`Bistro boss is siting on port ${port}`)
-} )
+  console.log(`Bistro boss is siting on port ${port}`)
+})
